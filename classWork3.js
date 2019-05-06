fa0ba2417b689ee346f4c582febfdd2a02f8c133
Maksim Pokhiliy <maksimpooh@gmail.com>
2019-05-06 13:00:08 +0300

    Update classWork3.js

--- classWork3.js
+++ classWork3.js
@@ -1,50 +1,50 @@
-// // task 1
-// function getAttributes() {
-//     let link = document.querySelector('a');
+// task 1
+function getAttributes() {
+    let link = document.querySelector('a');
 
-//     console.log(link.getAttribute('href'));
-//     console.log(link.getAttribute('hreflang'));
-//     console.log(link.getAttribute('rel'));
-//     console.log(link.getAttribute('target'));
-//     console.log(link.getAttribute('type'));
-// }
+    console.log(link.getAttribute('href'));
+    console.log(link.getAttribute('hreflang'));
+    console.log(link.getAttribute('rel'));
+    console.log(link.getAttribute('target'));
+    console.log(link.getAttribute('type'));
+}
 
-// // task 2
-// window.onload = function() {
-//     let table = document.querySelector('table');
+// task 2
+window.onload = function() {
+    let table = document.querySelector('table');
 
-//     for (let i = 0; i < table.rows.length; i++) {
-//       let row = table.rows[i];
-//       row.cells[i].style.backgroundColor = 'red';
-//     }
-// }
+    for (let i = 0; i < table.rows.length; i++) {
+      let row = table.rows[i];
+      row.cells[i].style.backgroundColor = 'red';
+    }
+}
 
-// // task 3
-// function createCloneNode(block) {
-//     let body = document.querySelector('body');
-//     let element = document.createElement(block);
-//     let p = document.createElement('p');
+// task 3
+function createCloneNode(block) {
+    let body = document.querySelector('body');
+    let element = document.createElement(block);
+    let p = document.createElement('p');
 
-//     p.textContent = 'Hello';
+    p.textContent = 'Hello';
 
-//     element.appendChild(p);
-//     body.appendChild(element);
-// }
-// createCloneNode('div');
-// createCloneNode('span');
+    element.appendChild(p);
+    body.appendChild(element);
+}
+createCloneNode('div');
+createCloneNode('span');
 
-// // task 4
-// function addChildrenTo(block, count, type) {
-//     let body = document.querySelector('body');
-//     let newBlock = document.createElement(block);
+// task 4
+function addChildrenTo(block, count, type) {
+    let body = document.querySelector('body');
+    let newBlock = document.createElement(block);
     
-//     for (let i = count; i > 0; i--) {
-//         let newType = document.createElement(type);
-//         body.appendChild(newBlock);
-//         newBlock.appendChild(newType);
-//     }
-// }
-// addChildrenTo('div', 3, 'p');
+    for (let i = count; i > 0; i--) {
+        let newType = document.createElement(type);
+        body.appendChild(newBlock);
+        newBlock.appendChild(newType);
+    }
+}
+addChildrenTo('div', 3, 'p');
 
 // task 5
 function setColor(text) {
